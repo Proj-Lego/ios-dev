7fbceaa547ec51b4f79f211e62fef5f0ed1ce8e8
Shreyas Patankar <sgpatankar@berkeley.edu>
2020-06-28 13:19:37 -0700

    sign up view draft

---
+++ Lego/Controller/Onboarding/NameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NameViewController.swift
+//  Lego
+//
+//  Created by Shreyas Patankar on 6/26/20.
+//  Copyright © 2020 Lego. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
